5d4ddb8b7f451935ef5fd72b52111d3ca48a167b
PGaillot <pr.gaillot@gmail.com>
2024-10-14 23:50:08 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,11 +11,11 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Installation des paquets nécessaires
-apt install -y dnsmasq hostapd dhcpcd5 netfilter-persistent iptables-persistent
+sudo apt install dnsmasq hostapd
 
 # Arrêt des services
-systemctl stop dnsmasq
-systemctl stop hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
 
 # Configuration de l'adresse IP statique
 cat << EOF >> /etc/dhcpcd.conf
@@ -26,17 +26,17 @@ interface wlan0
 EOF
 
 # Redémarrage du service dhcpcd
-service dhcpcd restart
+sudo service dhcpcd restart
 
 # Configuration de dnsmasq
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 cat << EOF > /etc/dnsmasq.conf
 interface=wlan0
 dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
 EOF
 
 # Démarrage de dnsmasq
-systemctl start dnsmasq
+sudo systemctl start dnsmasq
 
 # Configuration de hostapd
 cat << EOF > /etc/hostapd/hostapd.conf
@@ -53,23 +53,21 @@ rsn_pairwise=CCMP
 EOF
 
 # Indication de l'emplacement du fichier de configuration
-sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+sudo sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
 
 # Activation et démarrage de hostapd
-systemctl unmask hostapd
-systemctl enable hostapd
-systemctl start hostapd
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
 
 # Activation du routage
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 sysctl -p
 
 # Configuration du pare-feu
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
 
 # Sauvegarde des règles iptables
-netfilter-persistent save
+sudo netfilter-persistent save
 
 echo "Configuration terminée. Redémarrez votre Raspberry Pi pour appliquer les changements."
